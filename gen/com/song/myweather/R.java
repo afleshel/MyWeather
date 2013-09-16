b453846dd524726ee6fb187545a183af3501288f
songhongwang <1505803260@qq.com>
2013-09-16 12:14:07 +0800

    fix grivaty

--- gen/com/song/myweather/R.java
+++ gen/com/song/myweather/R.java
@@ -19,9 +19,8 @@ public final class R {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int refresh_icon=0x7f020002;
-        public static final int temp=0x7f020003;
-        public static final int widget_bg=0x7f020004;
-        public static final int widget_bg_portrait=0x7f020005;
+        public static final int widget_bg=0x7f020003;
+        public static final int widget_bg_portrait=0x7f020004;
     }
     public static final class id {
         public static final int menu_settings=0x7f090001;
